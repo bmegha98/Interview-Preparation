621d1ec0db8bec586c845c379b2dd173e5914426
Megha Bisht <bishtmegha98@gmail.com>
2021-02-23 16:20:05 +0530

    Using Two Queues and one queue

---
+++ Stack/LeetCode/Implement Stack using Queues.java
@@ -0,0 +1,103 @@
+/*
+Implement a last in first out (LIFO) stack using only two queues.The implemented stack should support all the functions of a normal queue(push, top, pop,and empty).
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+*/
+
+class MyStack {
+
+    private Queue<Integer> q1,q2;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        if(empty())
+            q1.offer(x);
+        else
+        {
+            while(!q1.isEmpty())
+                q2.offer(q1.poll());
+            q1.offer(x);
+            while(!q2.isEmpty())
+                q1.offer(q2.poll());
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return q1.poll();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return q1.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+ 
+ //Using only one queue
+ 
+ class MyStack {
+
+    private Queue<Integer> q;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q.offer(x);
+        int len = q.size();
+        while(len > 1)
+        {
+            q.offer(q.poll());
+            len--;
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return q.poll();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return q.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
