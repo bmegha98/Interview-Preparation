e984ad22da0f6ea918ba727fb469f47a4300f931
Megha Bisht <bishtmegha98@gmail.com>
2021-02-23 17:45:38 +0530

    Using Aux stack as well as Linked List

--- Stack/LeetCode/Min Stack.java
+++ Stack/LeetCode/Min Stack.java
@@ -37,3 +37,67 @@ class MinStack {
         return aux.peek();
     }
 }
+
+//Using Linked List
+
+class ListNode
+{
+    int val;
+    int min;
+    ListNode next;
+    public ListNode(int v,int m,ListNode ptr)
+    {
+        val = v;
+        min = m;
+        next = ptr;
+    }
+}
+class MinStack {
+
+    ListNode head;
+    int Min;
+    /** initialize your data structure here. */
+    public MinStack() {
+        head = null;
+        Min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int x) 
+    {
+        if(x < Min)
+            Min = x;
+        ListNode n = new ListNode(x,Min,head);
+        head = n;
+    }
+    
+    public void pop() {
+        
+        if(head.next != null)
+        {
+            Min = head.next.min;
+            head = head.next;
+        }
+        else
+        {
+            head = null;
+            Min = Integer.MAX_VALUE;
+        }
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return Min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
