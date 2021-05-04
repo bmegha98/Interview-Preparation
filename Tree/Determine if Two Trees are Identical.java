93dc6b7091a0613356ac661dced66bc5cd322ed1
Megha Bisht <bishtmegha98@gmail.com>
2021-05-04 19:00:57 +0530

    T(n) = Space = O(n)

---
+++ Tree/Determine if Two Trees are Identical.java
@@ -0,0 +1,17 @@
+//Given two binary trees, the task is to find if both of them are identical or not. 
+
+class Solution
+{
+    //Function to check if two trees are identical.
+	boolean isIdentical(Node root1, Node root2)
+	{
+	    // Code Here
+	    if(root1 == null && root2 == null)
+	        return true;
+	    if(root1 == null || root2 == null)
+	        return false;
+	    return (root1.data == root2.data)&&isIdentical(root1.left,root2.left)&&
+	    isIdentical(root1.right,root2.right);
+	}
+	
+}
