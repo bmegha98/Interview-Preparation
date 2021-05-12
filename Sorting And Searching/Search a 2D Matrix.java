0e3490678cd127fad398c9550af685102d8a2799
Megha Bisht <bishtmegha98@gmail.com>
2021-05-12 18:08:18 +0530

    Using Two Approaches

--- Sorting And Searching/Search a 2D Matrix.java
+++ Sorting And Searching/Search a 2D Matrix.java
@@ -24,3 +24,34 @@ class Solution {
         return false;
     }
 }
+
+//Time = O(logn)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        if(m == 1 && n == 1)
+            return matrix[0][0] == target;
+        
+        for(int i=0;i<m;i++) {
+            int[] A = matrix[i];
+            if(target >= A[0] && target <= A[n-1])
+                return binarySearch(A,n,target);
+        }
+        return false;
+    }
+    
+    private boolean binarySearch(int[] A,int n,int ele) {
+        int l = 0, h = n-1;
+        while(l <= h) {
+            int m = l+(h-l)/2;
+            if(A[m] == ele)
+                return true;
+            if(A[m] < ele)
+                l = m+1;
+            else
+                h = m-1;
+        }
+        return false;
+    }
+}
