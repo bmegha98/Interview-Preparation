ee8ce24500a41d8fb792b09862d7afc84b1e5588
Megha Bisht <bishtmegha98@gmail.com>
2021-05-13 18:11:34 +0530

    Constant Space

--- Sorting And Searching/Equilibrium Point.java
+++ Sorting And Searching/Equilibrium Point.java
@@ -3,6 +3,7 @@ Given an array A of n positive numbers. The task is to find the first Equilibium
 Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
 */
 
+//T(n) = O(n) , space = O(n)
 class Solution {
 
     
@@ -23,6 +24,36 @@ class Solution {
             long rightSum = sum-left[i]-arr[i];
             if(left[i]==rightSum)
                 return i+1;
+        }
+        return -1;
+    }
+}
+
+//T(n) = O(n) , space = O(1)
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return -1;
+        long left = arr[0];
+        long total = 0;
+        for(long ele : arr)
+            total += ele;
+            
+        for(int i=1;i<n;i++) {
+            long right = total-left-arr[i];
+            if(left == right)
+                return i+1;
+            left += arr[i];
         }
         return -1;
     }
