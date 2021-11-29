4a5e40a02dc4a49bbcf525f6bb94e6e25852fcad
Megha Bisht <bishtmegha98@gmail.com>
2021-11-29 18:06:02 +0530

    Using Binary Search

---
+++ Divide And Conquer/Find Minimum in Rotated Sorted Array II.java
@@ -0,0 +1,47 @@
+/*
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+
+[4,5,6,7,0,1,4] if it was rotated 4 times.
+[0,1,4,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
+
+You must decrease the overall operation steps as much as possible.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5]
+Output: 1
+Example 2:
+
+Input: nums = [2,2,2,0,1]
+Output: 0
+
+*/
+
+class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        if(n == 1)
+            return nums[0];
+        
+        int l = 0, h = n-1;
+        if(nums[l] < nums[h])
+            return nums[l];
+        
+        while(l<h) {
+            int m = l+(h-l)/2;
+            if(nums[m] == nums[h])
+                h--;
+            else if(nums[m] > nums[h])
+                l = m+1;
+            else
+                h = m;
+        }
+        
+        return nums[l];
+    }
+}
