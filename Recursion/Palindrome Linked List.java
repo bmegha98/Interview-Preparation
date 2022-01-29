d59c81ad4145054afa003b87d002d1d154a8d64d
Megha Bisht <bishtmegha98@gmail.com>
2022-01-29 14:17:27 +0530

    Recursive & Iterative

--- Recursion/Palindrome Linked List.java
+++ Recursion/Palindrome Linked List.java
@@ -20,3 +20,44 @@ class Solution {
         return res;
     }
 }
+
+//Iterative
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null)
+            return true;
+        ListNode first = head;
+        ListNode second = getSecondPart(head);
+        second = reverseList(second);
+        while(second != null) {
+            if(first.val != second.val)
+                return false;
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+    private ListNode getSecondPart(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while(fast != null) {
+            fast = fast.next;
+            if(fast != null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+        ListNode res = slow.next;
+        slow.next = null;
+        return res;
+    }
+    private ListNode reverseList(ListNode head) {
+        ListNode curr = head, prev = null, aux;
+        while(curr != null) {
+            aux = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = aux;
+        }
+        return prev;
+    }
+}
