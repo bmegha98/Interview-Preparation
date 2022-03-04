49e218ac14b6a50408821feeba72bdace4a3045b
Megha Bisht <bishtmegha98@gmail.com>
2022-03-04 15:30:47 +0530

    T(n) = Space = O(n)

---
+++ Stack/GFG/Largest Rectangle in Histogram.java
@@ -0,0 +1,47 @@
+/*
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the
+histogram.
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104
+*/
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        if(n == 0)
+            return 0;
+        int maxArea = 0,i=0;
+        
+        Stack<Integer> aux = new Stack<>();
+        for(;i<n;i++) {
+            while(!aux.isEmpty() && heights[i] < heights[aux.peek()]) {
+                int h = heights[aux.pop()];
+                int l = aux.isEmpty()?i:i-aux.peek()-1;
+                maxArea = Math.max(maxArea,l*h);
+            }
+            aux.push(i);
+        }
+        
+        while(!aux.isEmpty()) {
+            int h = heights[aux.pop()];
+            int l = aux.isEmpty()?i:i-aux.peek()-1;
+            maxArea = Math.max(maxArea,l*h);
+        }
+        return maxArea;
+    }
+}
