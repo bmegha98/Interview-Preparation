efba656217e21ab8b8ab241719ebb0d413638926
Megha Bisht <bishtmegha98@gmail.com>
2022-03-15 10:59:58 +0530

    T(n) = O(nlogk), Space = O(k)

---
+++ Queue/Kth Largest Element in an Array.java
@@ -0,0 +1,34 @@
+/*
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 104
+-104 <= nums[i] <= 104
+*/
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> aux = new PriorityQueue<>();
+        for(int ele : nums) {
+            aux.add(ele);
+            if(aux.size() > k)
+                aux.poll();
+        }
+        return aux.peek();
+    }
+}
