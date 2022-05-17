90d794db0fbad2d560cb5ba455f4c43fae18db23
Megha Bisht <bishtmegha98@gmail.com>
2022-05-17 12:14:34 +0530

    T(n) = O(n), Space = O(1)

--- Leetcode_random/Backspace String Compare.java
+++ Leetcode_random/Backspace String Compare.java
@@ -64,3 +64,47 @@ class Solution {
     }
 }
 
+//Space Optimization
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int m = s.length(), n = t.length();
+        
+        int i=m-1, j = n-1, skipS = 0, skipT = 0;
+        while(i>=0 || j>=0) {
+            while(i >= 0) {
+                if(s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                }
+                else if(skipS > 0) {
+                    skipS--;
+                    i--;
+                }
+                else
+                    break;
+            }
+            
+            while(j >= 0) {
+                if(t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                }
+                else if(skipT > 0) {
+                    skipT--;
+                    j--;
+                }
+                else
+                    break;
+            }
+            
+            if (i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j))
+                return false;
+            if ((i >= 0 && j < 0) || (i < 0 && j >= 0))
+                return false;
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
